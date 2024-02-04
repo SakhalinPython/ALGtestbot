4f60cd1761e96a7d968d4b2309d2ad53cb7613e6
Andrew B <101002859+Netdaem0n@users.noreply.github.com>
2024-02-04 16:51:08 +1100

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,66 +1,42 @@
-# pip install pyTelegramBotAPI
-# pip install qrcode
-
 import telebot
 from telebot import types
 from random import choice
 import qrcode
+import datetime
 
-token='6026848306:AAGCBYZy95arx9LbUWzZ8ZtxlPL1rxflVjM'
+token='5993558048:AAEwlZC5sc4eyFt9QgO1Js0Xgf9bmDoWRf0'
 
-bot=telebot.TeleBot(token)
-greetings = ['Привет!', 'Как дела?', 'Вацап', 'Бон жур!']
-image_urls = ['https://i.pinimg.com/736x/4f/87/56/4f875635ca282de2f683daf31ed0e91f.jpg',
-              'https://i.pinimg.com/236x/fc/9b/6c/fc9b6c7f71f11b4fa15ad08ea2e717e4.jpg',
-              'https://i.ytimg.com/vi/oFPxdL1vd-A/hqdefault.jpg']
-bot_users = []
+bot = telebot.TeleBot(token)
+greetings = ['Hello', 'Привет!', 'Как дела?', 'Че по чем!']
+users = dict()
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, choice(greetings))
-    bot.send_message(message.chat.id,'У нас есть комманды /start , /help , /qrcode, /who, /randimg')
-    user_id = message.from_user.id
-    user_nick = message.from_user.username
-    bot_users.append((user_id, user_nick))
-    bot.send_animation(message.chat.id, open('stupid-rat-pizza-tower.gif', 'rb'))
+    if message.from_user.id in users:
+        pass
+    else:
+        users[message.from_user.id] = [message.from_user.first_name, message.chat.id]
+    bot.send_message(message.chat.id, f"{message.from_user.first_name},"
+                                      f" {choice(greetings)}")
+    bot.send_message(message.chat.id, 'Доступные команды /time , /users')
 
-@bot.message_handler(commands=['randimg'])
-def start_message(message):
-    bot.send_message(message.chat.id,'Улыбнись!!')
-    bot.send_photo(message.chat.id, choice(image_urls))
+@bot.message_handler(commands=['time'])
+def current_time(message):
+    bot.send_message(message.chat.id, f'Твое время - {datetime.datetime.now()}')
+    bot.send_message(message.chat.id, 'cc')
 
-@bot.message_handler(commands=['help'])
-def start_message(message):
-    bot.send_message(message.chat.id,'Привет help')
-
-
-
-@bot.message_handler(commands=['qrcode'])
-def start_message(message):
-    bot.send_message(message.chat.id,'Привет qrcode')
-    bot.send_message(message.chat.id,'Сделать код - qr и ссылка, например "qr Http://ya.ru/"')
-    # qr = qrcode.QRCode()
-    # qr.add_data('Какие-то данные')
-    # img = qr.make_image()
-    # img.save(f'{message.from_user.id}_test.png')
-    # bot.send_photo(message.chat.id, open(f'{message.from_user.id}_test.png', 'rb'))
-
-@bot.message_handler(commands=['who'])
-def start_message(message):
-    for id, name in bot_users:
-        bot.send_message(message.chat.id, f'{id} - {name}')
+@bot.message_handler(commands=['users'])
+def current_time(message):
+    if users:
+        for id, nickname in users.items():
+            bot.send_message(message.chat.id, f'{id} - {nickname[0]}')
+    else:
+        bot.send_message(message.chat.id, f'Список пользователей пуст!')
 
 @bot.message_handler(content_types='text')
-def start_message(message):
-    temp = message.text.split(" ")
-
-    if message.text.lower() == 'картинка':
-        bot.send_message(message.chat.id, 'Твоя картинка')
-        bot.send_photo(message.chat.id, open('h1.jpg', 'rb'))
-    elif message.text.lower() == 'мем':
-        bot.send_message(message.chat.id, 'Твой мем')
-        bot.send_video(message.chat.id, open('video_2024-01-14_14-09-10.mp4', 'rb'))
-    elif temp[0].lower() == 'qr':
+def my_text(message):
+    temp = message.text.split(' ')
+    if temp[0].lower() == 'qr' and len(temp) >= 2:
         bot.send_message(message.chat.id, f'Твоя ссылка - {temp[1]}')
         qr = qrcode.QRCode()
         qr.add_data(temp[1])
@@ -68,13 +44,15 @@ def start_message(message):
         img.save(f'{message.from_user.id}_test.png')
         bot.send_photo(message.chat.id, open(f'{message.from_user.id}_test.png', 'rb'))
 
-@bot.message_handler(content_types='voice')
-def start_message(message):
-    bot.send_message(message.chat.id, 'Вы прислали голосовое')
-    data = bot.get_file(message.voice.file_id)
-    downloaded_file = bot.download_file(data.file_path)
-    with open(f'{message.from_user.username}_test.ogg', 'wb') as fl:
-        fl.write(downloaded_file)
 
+    if message.text.lower() == 'покажи список':
+        bot.send_message(message.chat.id, '1) Команда 1 (qr) \n2) Команда 2 (mem)\n3) Команда 3 (txt)')
+    if message.text.lower() == 'qr':
+        bot.send_message(message.chat.id, 'Пиши команду qr и ссылку через пробел')
+        bot.send_message(message.chat.id, 'qr Https://ya.ru/')
+    elif message.text.lower() == 'mem':
+        print('mem')
+    elif message.text.lower() == 'txt':
+        print('txt')
 
 bot.infinity_polling()
