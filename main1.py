efabfa294a347fe352febdbfc8e26c89037ccd01
Andrew B <101002859+Netdaem0n@users.noreply.github.com>
2024-02-04 16:57:11 +1100

    Update main1.py

--- main1.py
+++ main1.py
@@ -4,6 +4,9 @@ from random import choice
 import qrcode
 import datetime
 
+
+start_time = datetime.datetime.now()
+
 token='5993558048:AAEwlZC5sc4eyFt9QgO1Js0Xgf9bmDoWRf0'
 
 bot = telebot.TeleBot(token)
@@ -18,15 +21,33 @@ def start_message(message):
         users[message.from_user.id] = [message.from_user.first_name, message.chat.id]
     bot.send_message(message.chat.id, f"{message.from_user.first_name},"
                                       f" {choice(greetings)}")
-    bot.send_message(message.chat.id, 'Доступные команды /time')
+    bot.send_message(message.chat.id, 'Доступные команды /time , /users')
 
 @bot.message_handler(commands=['time'])
 def current_time(message):
     bot.send_message(message.chat.id, f'Твое время - {datetime.datetime.now()}')
-    bot.send_message(message.chat.id, 'cc')
+    bot.send_message(message.chat.id, f'Время с запуска {datetime.datetime.now() - start_time}')
+
+@bot.message_handler(commands=['users'])
+def current_time(message):
+    if users:
+        for id, nickname in users.items():
+            bot.send_message(message.chat.id, f'{id} - {nickname[0]}')
+    else:
+        bot.send_message(message.chat.id, f'Список пользователей пуст!')
 
 @bot.message_handler(content_types='text')
 def my_text(message):
+    temp = message.text.split(' ')
+    if temp[0].lower() == 'qr' and len(temp) >= 2:
+        bot.send_message(message.chat.id, f'Твоя ссылка - {temp[1]}')
+        qr = qrcode.QRCode()
+        qr.add_data(temp[1])
+        img = qr.make_image()
+        img.save(f'{message.from_user.id}_test.png')
+        bot.send_photo(message.chat.id, open(f'{message.from_user.id}_test.png', 'rb'))
+
+
     if message.text.lower() == 'покажи список':
         bot.send_message(message.chat.id, '1) Команда 1 (qr) \n2) Команда 2 (mem)\n3) Команда 3 (txt)')
     if message.text.lower() == 'qr':
@@ -38,3 +59,4 @@ def my_text(message):
         print('txt')
 
 bot.infinity_polling()
+
