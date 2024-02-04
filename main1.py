e328c0bd623f46ad9e3179f32881cc10d1195ca6
Andrew B <101002859+Netdaem0n@users.noreply.github.com>
2024-02-04 16:24:03 +1100

    Update main1.py

--- main1.py
+++ main1.py
@@ -2,14 +2,28 @@ import telebot
 from telebot import types
 from random import choice
 import qrcode
+import datetime
 
-token='ВАШ ТОКЕН'
+token='5993558048:AAEwlZC5sc4eyFt9QgO1Js0Xgf9bmDoWRf0'
 
 bot = telebot.TeleBot(token)
+greetings = ['Hello', 'Привет!', 'Как дела?', 'Че по чем!']
+users = dict()
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, choice("Привет"))
-    bot.send_message(message.chat.id,'У нас есть комманды /start , /help , /qrcode, /who, /randimg')
+    if message.from_user.id in users:
+        pass
+    else:
+        users[message.from_user.id] = [message.from_user.first_name, message.chat.id]
+    bot.send_message(message.chat.id, f"{message.from_user.first_name},"
+                                      f" {choice(greetings)}")
+    bot.send_message(message.chat.id, 'Доступные команды /time')
+
+@bot.message_handler(commands=['time'])
+def current_time(message):
+    bot.send_message(message.chat.id, f'Твое время - {datetime.datetime.now()}')
+
+
 
 bot.infinity_polling()
